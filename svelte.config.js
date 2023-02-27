e4e5674cb6d7a8218ebbcc50b21faa06b73834e7
ruggiadufour <69329924+ruggiadufour@users.noreply.github.com>
2023-02-27 19:25:30 -0300

    feat: added vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 // import preprocess from 'svelte-preprocess'
-import seqPreprocessor from 'svelte-sequential-preprocessor'
-import { preprocessThrelte } from '@threlte/preprocess'
-
-
+import seqPreprocessor from 'svelte-sequential-preprocessor';
+import { preprocessThrelte } from '@threlte/preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -16,7 +15,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: vercel()
 	}
 };
 
